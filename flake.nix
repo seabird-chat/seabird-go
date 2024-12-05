1af90eb9b99f5f9c14af9965fc869efb0774a9b9
Kaleb Elwert <belak@coded.io>
2024-12-04 22:53:51 -0800

    Update protos to include message blocks

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, devshell, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+
+            overlays = [
+              devshell.overlays.default
+            ];
+          };
+        in
+        pkgs.devshell.mkShell {
+          devshell.motd = "";
+
+          devshell.packages = [
+            pkgs.go
+            pkgs.protobuf
+            pkgs.protoc-gen-go
+            pkgs.protoc-gen-go-grpc
+          ];
+        };
+    });
+}
